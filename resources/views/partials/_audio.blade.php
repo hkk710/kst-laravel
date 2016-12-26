7693ea9c641ddc066b59ddb880f7f4cfaa17240d
Advaith3600 <advaitharunjeena@gmail.com>
2016-12-26 14:03:45 +0530

    Audio added

--- resources/views/partials/_audio.blade.php
+++ resources/views/partials/_audio.blade.php
@@ -1,3 +1,6 @@
 <audio hidden id="audio">
-	<source src="{{asset('sounds/click.mp3')}}">
+	<source src="{{ asset('sounds/click.mp3') }}" type="audio/mp3">
+</audio>
+<audio hidden loop autoplay>
+	<source src="{{ asset('sounds/back.mp3') }}" type="audio/mp3">
 </audio>
