7e1e586e3c12e55826d56845a6d6e0efc18b4bdf
Advaith3600 <advaitharunjeena@gmail.com>
2017-03-04 18:51:29 +0530

    Instamojo updated

--- resources/views/online_vazhipad/instamojo/pay.php
+++ resources/views/online_vazhipad/instamojo/pay.php
@@ -3,37 +3,25 @@
     use App\Vname;
 
     $vname = Vname::find($request->vname);
-    $product_name = $vname->name;
-    $price = $vname->price;
-    $name = $request->name;
-    $email = Auth::user()->email;
-
-
-    include 'src/instamojo.php';
-
-    $api = new Instamojo\Instamojo('40c79482174c9cc0f8fd8b54e77ab38e', '74420e94c0f2d8cc2a46626d7a13a882','https://test.instamojo.com/api/1.1/');
-
+    $api = new Instamojo\Instamojo('40c79482174c9cc0f8fd8b54e77ab38e', '74420e94c0f2d8cc2a46626d7a13a882', 'https://test.instamojo.com/api/1.1/');
 
     try {
         $response = $api->paymentRequestCreate(array(
-            "purpose" => $product_name,
-            "amount" => $price,
-            "buyer_name" => $name,
+            "purpose" => "Online Vazhipad",
+            "amount" => $vname->price,
+            "buyer_name" => $request->name,
             "send_email" => true,
-            "email" => $email,
-            'allow_repeated_payments' => false,
-            "redirect_url" => url('/') . "/online_vazhipad/thankyou",
-            "webhook" =>  url('/') . "/online_vazhipad/webhook"
-            ));
-        //print_r($response);
-
+            "email" => Auth::user()->email,
+            "allow_repeated_payments" => false,
+            "redirect_url" => url('/online_vazhipad/thankyou'),
+            "webhook" => url('/online_vazhipad/webhook')
+        ));
         $pay_ulr = $response['longurl'];
 
         //Redirect($response['longurl'],302); //Go to Payment page
 
         header("Location: $pay_ulr");
         exit();
-
     }
     catch (Exception $e) {
         print('Error: ' . $e->getMessage());
--- resources/views/online_vazhipad/instamojo/thankyou.php
+++ resources/views/online_vazhipad/instamojo/thankyou.php
@@ -1,71 +1,10 @@
-
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Thank You, Mojo</title>
-<link rel="stylesheet" href="/css/app.css" media="screen" title="no title">
-<script type="text/javascript" src="js/app.js">
-
-</script>
-</head>
-
-  <body>
-    <div class="container">
-
-      <div class="page-header">
-        <h1><a href="/mojo">Instamojo Payment</a></h1>
-        <p class="lead">A test payment integration for instamojo paypemnt gateway. Written in PHP</p>
-      </div>
-
-      <h3 style="color:#6da552">Thank You, Payment succus!!</h3>
-
-
-
-  <?php
-
-include 'src/instamojo.php';
-
-$api = new Instamojo\Instamojo('40c79482174c9cc0f8fd8b54e77ab38e', '74420e94c0f2d8cc2a46626d7a13a882','https://test.instamojo.com/api/1.1/');
-
-$payid = $_GET["payment_request_id"];
-
-
-try {
-    $response = $api->paymentRequestStatus($payid);
-
-
-    echo "<h4>Payment ID: " . $response['payments'][0]['payment_id'] . "</h4>" ;
-    echo "<h4>Payment Name: " . $response['payments'][0]['buyer_name'] . "</h4>" ;
-    echo "<h4>Payment Email: " . $response['payments'][0]['buyer_email'] . "</h4>" ;
-
-  echo "<pre>";
-   print_r($response);
-echo "</pre>";
-    ?>
-
-
-    <?php
-}
-catch (Exception $e) {
-    print('Error: ' . $e->getMessage());
-}
-
-
-
-  ?>
-
-
-
-    </div> <!-- /container -->
-
-
-  </body>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Thank You</title>
+    </head>
+    <body>
+        <h1>ThankYou!</h1>
+    </body>
 </html>
